8e0e53a41c297de396673a1e15ff711e21827860
Kenneth Griggs <kenrgriggs@gmail.com>
2021-02-04 20:05:08 -0500

    blork

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 // import TenLittleMonkeys from './components/challenges/day03/TenLittleMonkeys'
-import Auth from './components/challenges/day03/Auth/Auth';
-
+// import Auth from "./components/challenges/day03/Auth/Auth";
+import MortyParent from "./components/morty/MortyParent";
 
 
 function App() {
   return (
     <div>
-      <Auth />
+
+        <MortyParent />
+
     </div>
   );
 }
